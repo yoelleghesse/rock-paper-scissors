31decf479a7ac0fde57d42b8a000e1e598a4134c
Yoel Leghesse <yoelleghesse@gmail.com>
2025-09-18 16:30:00 -0400

    initialized computer choice logic

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 0) {
+        return "rock";
+    } else if (computerChoice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+
+}
