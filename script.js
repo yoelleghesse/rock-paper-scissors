4130c86802955d8709a93b6b371d6a08077a68fe
Yoel Leghesse <yoelleghesse@gmail.com>
2025-09-18 16:48:26 -0400

    added player func and round logic

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice === 0) {
-        return "rock";
+        console.log("rock");
     } else if (computerChoice === 1) {
-        return "paper";
+        console.log("paper");
     } else {
-        return "scissors";
+        console.log("scissors");
     }
 
+}
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Enter rock, paper, or scissors:");
+    return playerChoice;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const compterChoice = getComputerChoice();
+const playerChoice = getPlayerChoice();
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === compterChoice) {
+        return "It's a tie!"
+    } else if (playerChoice.toLowerCase() === "rock" && computerChoice === "scissors") {
+        humanScore++;
+        return "You win! Rock beats scissors."
+    } else if (playerChoice.toLowerCase() === "paper" && computerChoice === "rock") {
+        humanScore++;
+        return "You win! Paper beats rock."
+    } else if (playerChoice.toLowerCase() === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        return "You win! Scissors beats paper."
+    } else {
+        computerScore++;
+        return `You lose! ${computerChoice} beats ${playerChoice}.`
+    }
 }
